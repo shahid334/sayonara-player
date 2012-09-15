8ae60cfe8f28f1e09e70236968d27db60b296afe
LucioCarreras <LucioCarreras@229943ad-b952-e4d0-2def-3ce31e46261e>
2012-09-15 11:04:30 +0000

    update

--- trunk/linux_packages/sayonara2debian.sh
+++ trunk/linux_packages/sayonara2debian.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-
+#!/bin/basha
+OUTPUT_FILE=sayonara-0.3.deb
+CONTROL_FILE=./resources/sayonara32.control
 DEBIAN_DIR=sayonara.debian
 CUR_DIR=$PWD
 
@@ -7,6 +8,12 @@ if [ "$1" = "clean" ] ; then
 	rm -rf $DEBIAN_DIR
 	exit
 fi
+
+if [ "$1" = "-ax64" ] ; then
+	CONTROL_FILE=./resources/sayonara64.control
+	OUTPUT_FILE=sayonara-0.3_amd64.dev
+fi
+	
 
 mkdir -p $DEBIAN_DIR/DEBIAN
 mkdir -p $DEBIAN_DIR/usr/share/sayonara
@@ -29,10 +36,10 @@ cp ../empty.db $DEBIAN_DIR/usr/share/sayonara/player.db
 cp ../empty.db $DEBIAN_DIR/usr/share/sayonara/empty.db
 cp ./resources/sayonara.postinst $DEBIAN_DIR
 cp ./resources/sayonara.prerm $DEBIAN_DIR
-cp ./resources/sayonara.control $DEBIAN_DIR/DEBIAN/control
+cp $CONTROL_FILE $DEBIAN_DIR/DEBIAN/control
 cp ./resources/sayonara.desktop $DEBIAN_DIR/usr/share/applications
 cp ../Engine/GStreamer/libsayonara_gstreamer.so $DEBIAN_DIR/usr/lib/sayonara
 
-dpkg-deb -b $DEBIAN_DIR $DEBIAN_DIR/sayonara-0.3.deb
+dpkg-deb -b $DEBIAN_DIR $DEBIAN_DIR/$OUTPUT_FILE
 echo Written to $DEBIAN_DIR/sayonara-0.3.deb
 
