2fed441956083d155def04ab529aa8838bac89fe
LucioCarreras <LucioCarreras@229943ad-b952-e4d0-2def-3ce31e46261e>
2012-11-21 22:58:03 +0000

    playlists export

---
+++ trunk/GUI/TargetPlaylistDialog/GUI_Target_Playlist_Dialog.cpp
@@ -0,0 +1,55 @@
+#include "ui_GUI_Target_Playlist_Dialog.h"
+#include "GUI/TargetPlaylistDialog/GUI_Target_Playlist_Dialog.h"
+#include "HelperStructs/Style.h"
+#include "HelperStructs/CSettingsStorage.h"
+
+#include <QFileDialog>
+
+GUI_Target_Playlist_Dialog::GUI_Target_Playlist_Dialog(QWidget *parent) :
+    QDialog(parent)
+{
+    ui = new Ui_GUI_Target_Playlist_Dialog();
+    ui->setupUi(this);
+
+    connect(ui->btn_choose, SIGNAL(clicked()), this, SLOT(search_button_clicked()));
+    connect(ui->btn_ok, SIGNAL(clicked()), this, SLOT(ok_button_clicked()));
+}
+
+GUI_Target_Playlist_Dialog::~GUI_Target_Playlist_Dialog(){
+
+}
+
+
+void GUI_Target_Playlist_Dialog::change_skin(bool dark){
+
+    QString btn_style = Style::get_pushbutton_style(dark);
+    QString le_style = Style::get_lineedit_style(dark);
+    this->ui->btn_cancel->setStyleSheet(btn_style);
+    this->ui->btn_ok->setStyleSheet(btn_style);
+    this->ui->btn_choose->setStyleSheet(btn_style);
+    this->ui->le_path->setStyleSheet(le_style);
+}
+
+
+
+void GUI_Target_Playlist_Dialog::search_button_clicked(){
+    QString target_filename = QFileDialog::getSaveFileName(this,
+                                                           "Choose target file",
+                                                           CSettingsStorage::getInstance()->getLibraryPath(),
+                                                           "*.m3u");
+
+    if(!target_filename.endsWith("m3u", Qt::CaseInsensitive)) target_filename.append(".m3u");
+    this->ui->le_path->setText(target_filename);
+}
+
+
+void GUI_Target_Playlist_Dialog::ok_button_clicked(){
+    QString target_filename = ui->le_path->text();
+    bool checked = ui->cb_relative->isChecked();
+
+    if(target_filename.size() > 0){
+        emit sig_target_chosen(target_filename, checked);
+        close();
+    }
+
+}
