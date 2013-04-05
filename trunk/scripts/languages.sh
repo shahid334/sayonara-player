b0be523d6f09f490a9837ed90cddc03b292263a0
LucioCarreras <LucioCarreras@229943ad-b952-e4d0-2def-3ce31e46261e>
2013-04-05 15:20:09 +0000

    languages

--- trunk/scripts/languages.sh
+++ trunk/scripts/languages.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-for f in ./languages/* ; do
+if [ $# -eq 0 ] ; then
+	echo "$0 update|release"
+	exit
+fi  
 
-	lupdate . -ts ./$f
 
-done
+if [ "$1" = "update" ] ; then
+
+	for f in ./languages/*.ts ; do
+
+		lupdate . -ts ./$f
+
+	done	
+
+elif [ "$1" = "release" ] ; then
+	for f in ./languages/*.ts ; do
+		BASE=$(basename $f)
+		FILENAME="${BASE%.*}"
+		lrelease -removeidentical $f -qm languages/${FILENAME}.qm
+	done
+
+else
+	echo bla
+
+
+fi
+		
