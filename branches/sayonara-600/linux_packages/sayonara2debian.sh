a6638642039b1539a6a32064750092e684f56f9b
LucioCarreras <LucioCarreras@229943ad-b952-e4d0-2def-3ce31e46261e>
2013-04-16 13:01:11 +0000

    moving to src

--- branches/sayonara-600/linux_packages/sayonara2debian.sh
+++ branches/sayonara-600/linux_packages/sayonara2debian.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ $# -eq 0 ] ; then
+	echo "Specifiy one of these files:"
+	find ./resources -name "*.control"
+	exit
+fi
+
 cd ..
 sh scripts/setversion.sh
-
 
 STR_MAJOR=`grep -e "^MAJOR" ./VERSION`
 STR_MINOR=`grep -e "^MINOR" ./VERSION`
@@ -18,6 +23,11 @@ ARCH=`uname -m`
 
 
 cd linux_packages
+
+ROOT=".."
+SRC="${ROOT}/src/"
+BIN="${ROOT}/bin/"
+LIB="${ROOT}/lib/"
 
 OUTPUT_FILE="sayonara-${VER_MAJOR}.${VER_MINOR}.${VER_SUBMINOR}-r${VER_BUILD}-${ARCH}.deb"
 
@@ -62,7 +72,7 @@ cd $CUR_DIR
 cp $CONTROL_FILE $DEBIAN_DIR/DEBIAN/control
 cp ./resources/sayonara.prerm    $DEBIAN_DIR/DEBIAN/prerm
 cp ./resources/sayonara.postinst $DEBIAN_DIR/DEBIAN/postinst
-cp ./resources/copyright         ${DEBIAN_DIR}/usr/share/doc/sayonara/copyright
+cp ./resources/copyright         $DEBIAN_DIR/usr/share/doc/sayonara/copyright
 
 # linux stuff
 
@@ -70,26 +80,28 @@ cp -r ./resources/logos/*    $DEBIAN_DIR/usr/share/icons/hicolor
 find $DEBIAN_DIR/usr/share/icons/hicolor -iname "*svn*" | xargs rm -rf
 cp ./resources/sayonara      $DEBIAN_DIR/usr/share/menu
 
-cp ../GUI/icons/sayonara.png ${DEBIAN_DIR}/usr/share/pixmaps
-cp ../GUI/icons/sayonara.png ${DEBIAN_DIR}/usr/share/icons
-cp ../GUI/icons/*            $DEBIAN_DIR/usr/share/sayonara
+cp $SRC/GUI/icons/sayonara.png ${DEBIAN_DIR}/usr/share/pixmaps
+cp $SRC/GUI/icons/sayonara.png ${DEBIAN_DIR}/usr/share/icons
+cp $SRC/GUI/icons/*            $DEBIAN_DIR/usr/share/sayonara
+cp $SRC/GUI/icons/sayonara.xpm $DEBIAN_DIR/usr/share/sayonara/sayonara.xpm
+
 chmod -x $DEBIAN_DIR/usr/share/sayonara/logo.png
-cp ../GUI/icons/sayonara.xpm $DEBIAN_DIR/usr/share/sayonara/sayonara.xpm
 
 # sayonara stuff
-cp ../VERSION $DEBIAN_DIR/usr/share/sayonara
-cp ../empty.db $DEBIAN_DIR/usr/share/sayonara/player.db
-cp ../empty.db $DEBIAN_DIR/usr/share/sayonara/empty.db
-cp ../GUI/dark.css $DEBIAN_DIR/usr/share/sayonara/dark.css
-cp ../GUI/standard.css $DEBIAN_DIR/usr/share/sayonara/standard.css
+cp $ROOT/VERSION $DEBIAN_DIR/usr/share/sayonara
+cp $ROOT/empty.db $DEBIAN_DIR/usr/share/sayonara/player.db
+cp $ROOT/empty.db $DEBIAN_DIR/usr/share/sayonara/empty.db
+cp $SRC/GUI/dark.css $DEBIAN_DIR/usr/share/sayonara/dark.css
+cp $SRC/GUI/standard.css $DEBIAN_DIR/usr/share/sayonara/standard.css
 cp ./resources/sayonara.desktop $DEBIAN_DIR/usr/share/applications
-cp ../*.qm $DEBIAN_DIR/usr/share/sayonara/translations
-cp -r ../languages/icons $DEBIAN_DIR/usr/share/sayonara/translations
-cp ../Engine/GStreamer/libsayonara_gstreamer.so $DEBIAN_DIR/usr/lib/sayonara
+cp $SRC/*.qm $DEBIAN_DIR/usr/share/sayonara/translations
+cp -r $SRC/languages/icons $DEBIAN_DIR/usr/share/sayonara/translations
+cp $LIB/libsayonara_gstreamer.so $DEBIAN_DIR/usr/lib/sayonara
+cp $LIB/libsayonara_libnotify.so $DEBIAN_DIR/usr/lib/sayonara
+cp $BIN/sayonara $DEBIAN_DIR/usr/bin
+
 chmod -x $DEBIAN_DIR/usr/lib/sayonara/libsayonara_gstreamer.so
-cp ../Notification/libnotify/libsayonara_libnotify.so $DEBIAN_DIR/usr/lib/sayonara
 chmod -x $DEBIAN_DIR/usr/lib/sayonara/libsayonara_libnotify.so
-cp ../bin/sayonara $DEBIAN_DIR/usr/bin
 
 
 gzip --best -c resources/changelog > $DEBIAN_DIR/usr/share/doc/sayonara/changelog.gz 
