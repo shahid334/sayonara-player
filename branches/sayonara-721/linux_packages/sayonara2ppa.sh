6a944ad1600bfea1abba4e7d62975395112ea850
LucioCarreras <LucioCarreras@229943ad-b952-e4d0-2def-3ce31e46261e>
2013-07-30 14:50:38 +0000

    update

---
+++ branches/sayonara-721/linux_packages/sayonara2ppa.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+svn update
+
+
+VERSION=$1
+KEYFILE=$2
+TARGET_FILENAME="sayonara-${VERSION}"
+TARGET_PATH="./${TARGET_FILENAME}-ppa"
+DEBIAN_DIR=${TARGET_PATH}/debian
+APP_DIR=${TARGET_PATH}/applications
+
+echo "Checking out..."
+svn export "https://sayonara-player.googlecode.com/svn/trunk" ${TARGET_PATH} 
+
+mkdir $DEBIAN_DIR
+mkdir $APP_DIR
+
+cp resources/ppa.control ${DEBIAN_DIR}
+cp resources/changelog ${DEBIAN_DIR}
+cp resources/copyright ${DEBIAN_DIR}
+cp resources/rules ${DEBIAN_DIR}
+echo 7 > ${DEBIAN_DIR}/compat
+
+cp ../src/GUI/icons/sayonara.xpm ${APP_DIR}
+grep ${VERSION} ${DEBIAN_DIR}/changelog
+RET=$?
+
+if [ $RET -gt 0 ] ; then
+	echo "VERSION in changelog is not ${VERSION}"
+	exit
+fi
+
+cd $TARGET_PATH
+
+KEY=`echo ${KEYFILE}`
+debuild -S -k\"${KEY}\"
+RET=$?
+
+if [ ${RET} -ne 0 ] ; then
+        echo "debuild failed"
+        exit
+fi
+
+cd ..
+
+echo "Upload? y/n"
+UPLOAD=n
+read UPLOAD
+if [ "$UPLOAD" == "y" ] ; then
+	dput ppa:lucioc/sayonara sayonara_${VERSION}_source.changes
+fi
+
+
